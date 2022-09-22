a3154db9f5cdb5e8a607cacf11dd1b9750af7e25
Fernando <wilmercaiza@gmail.com>
2022-09-22 08:32:26 -0500

    Finish fizzbuzz kata

--- src/main/java/com/cgm/tdd/katas/FizzBuzz.java
+++ src/main/java/com/cgm/tdd/katas/FizzBuzz.java
@@ -2,11 +2,31 @@ package com.cgm.tdd.katas;
 
 import lombok.NoArgsConstructor;
 
+import java.util.function.BiPredicate;
+
 @NoArgsConstructor
 public class FizzBuzz {
 
+    public static final String FIZZ = "fizz";
+    public static final String BUZZ = "buzz";
+    public static final String FIZZ_BUZZ = FIZZ+BUZZ;
 
     public String execute (final int max) {
+
+        BiPredicate<Integer, Integer> isDivisibleBy = (Integer divider, Integer n) -> n % divider == 0;
+
+        if (Boolean.TRUE.equals(isDivisibleBy.test(35, max))) {
+            return FIZZ_BUZZ;
+        }
+
+        if (Boolean.TRUE.equals(isDivisibleBy.test(5, max))) {
+            return FIZZ;
+        }
+
+        if (Boolean.TRUE.equals(isDivisibleBy.test(7, max))) {
+            return BUZZ;
+        }
+
         return String.valueOf(max);
     }
 
--- src/test/java/com/cgm/tdd/katas/FizzBuzzTest.java
+++ src/test/java/com/cgm/tdd/katas/FizzBuzzTest.java
@@ -23,4 +23,65 @@ class FizzBuzzTest {
 
     }
 
+    @Test
+    void should_return_fizz_if_receive_five() {
+
+        final var max = 5;
+        var result = this.fizzBuzz.execute(max);
+        assertEquals(FizzBuzz.FIZZ, result);
+
+    }
+
+    @Test
+    void should_return_buzz_if_receive_seven() {
+
+        final var max = 7;
+        var result = this.fizzBuzz.execute(max);
+        assertEquals(FizzBuzz.BUZZ, result);
+
+    }
+
+    @Test
+    void should_return_fizzbuzz_if_receive_thirty_five() {
+
+        final var max = 35;
+        var result = this.fizzBuzz.execute(max);
+        assertEquals(FizzBuzz.FIZZ_BUZZ, result);
+
+    }
+
+    @Test
+    void should_return_fizz_if_receive_any_number_divisible_by_five () {
+
+        final var max = 15;
+        var result = this.fizzBuzz.execute(max);
+        assertEquals(FizzBuzz.FIZZ, result);
+
+    }
+
+    @Test
+    void should_return_fizz_if_receive_any_number_divisible_by_seven() {
+
+        final var max = 21;
+        var result = this.fizzBuzz.execute(max);
+        assertEquals(FizzBuzz.BUZZ, result);
+
+    }
+
+    @Test
+    void should_return_fizzbuzz_if_receive_any_number_divisible_by_fifteen() {
+        final var max = 105;
+        var result = this.fizzBuzz.execute(max);
+        assertEquals(FizzBuzz.FIZZ_BUZZ, result);
+    }
+
+    @Test
+    void should_return_the_same_number_that_receives() {
+
+        final var max = 9;
+        var result = this.fizzBuzz.execute(max);
+        assertEquals(String.valueOf(max), result);
+
+    }
+
 }
